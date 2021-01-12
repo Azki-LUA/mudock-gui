31b9541474006ddbec08708a5909ccbc4da74492
Azki-LUA <74989734+Azki-LUA@users.noreply.github.com>
2021-01-12 12:53:20 -0500

    Update mudockgui.lua

--- mudockgui.lua
+++ mudockgui.lua
@@ -1,1 +1,13 @@
-
+local UserInputService = game:GetService'UserInputService';
+local HttpService = game:GetService'HttpService';
+local GUIService = game:GetService'GuiService';
+local TweenService = game:GetService'TweenService';
+local RunService = game:GetService'RunService';
+local Players = game:GetService'Players';
+local LocalPlayer = Players.LocalPlayer;
+local Camera = workspace.CurrentCamera;
+local Mouse = LocalPlayer:GetMouse();
+local player = game.Players.LocalPlayer
+local Player = game.Players.LocalPlayer
+local name = player.Name
+local Name = Player.Name
